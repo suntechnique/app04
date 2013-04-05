69e416758e132a69e50c7d40057aa1b9fc6c8f47
vsting <faetonvs@gmail.com>
2013-04-05 16:32:49 +0600

    w 05.04.13 16:26

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App04::Application.config.secret_token = 'c1de3b46a308013f754d5c65f7c771a359053dbeb8ebca9bb26132625d6c2c9725ea19fdfd11e3ccdb1475bc417d1e3bd9e54808f2077426a6e92772683e2762'
