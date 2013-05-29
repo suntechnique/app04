be9f85556b1c619f94d379c94dfc6c7d4f0df0c4
vsting <faetonvs@gmail.com>
2013-05-29 16:52:58 +0600

    SIO 29.05.2013 16:53

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+	def new
+	end
+
+	def create
+		user = User.find_by_email(params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			# Sign the user in and redirect to the user's show page.
+		else
+			# Create an error message and re-render the signin form.
+		end
+	end
+
+	def destroy
+	end
+end
