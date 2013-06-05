13d41f7a8b1875f15df7fb8d59940f5de5006d52
vsting <faetonvs@gmail.com>
2013-06-05 14:57:54 +0600

    uu 05.05.2013 14:57

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(:version => 20130604094922) do
     t.string   "password_digest"
     t.string   "remember_token"
     t.boolean  "admin",           :default => false
-    t.string   "avatar"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
